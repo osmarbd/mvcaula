7188aac1657ad7660bed63d3be3f6b7c8b4e7049
Osmar Betazzi Dordal <osmarbd@gmail.com>
2024-09-25 19:22:25 -0300

    pass

--- src/DB/Connection.php
+++ src/DB/Connection.php
@@ -16,9 +16,9 @@ class Connection
     public static function getInstance()
     {
         if(is_null(self::$instance)){
-            self::$instance = new \PDO('mysql:dbname=sitecatalogo;host=127.0.0.1;port=3307',
-                'root',
-                'obtz76');
+            self::$instance = new \PDO('mysql:dbname=<dbname>;host=127.0.0.1;port=<330x>',
+                '<user>',
+                '<pwd>');
             self::$instance->exec('SET NAMES UTF8');
         }
         return self::$instance;
